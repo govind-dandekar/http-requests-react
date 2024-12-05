e3f54e151f6b49cc2038ced0dc8616be4edc0803
Govind Dandekar <govind@Govinds-Mini.lan>
2024-12-05 12:20:00 -0800

    add isFetching state to Available places to manage data loading UI

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,22 +4,29 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   // can use Fetch to send data too
   // updated to async await: can create async fx in uE
   useEffect(() => {
     async function fetchPlaces(){
+      setIsFetching(true);
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
-      setAvailablePlaces(resData)
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
+
+    fetchPlaces();
   },[])
-  
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
